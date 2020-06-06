2bb8819245c03a47e58360ad3baa3657071c2fd2
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-06-06 17:08:50 +0100

    actually rec proper duration

--- record_stream.py
+++ record_stream.py
@@ -5,7 +5,7 @@ import threading
 import time
 import sys
 
-def record_hour(filename, ext, stream_url):
+def record_hour(filename, ext, stream_url, record_duration):
     # Example:
     # $ ffmpeg -i "https://d2od87akyl46nm.cloudfront.net/live/omroepgelderland/kijkradio/index.m3u8" -c:a copy -filter:v scale=420:-1 -crf 35 -t 00:00:05 file.mp4
     cmd_video = [
@@ -20,7 +20,7 @@ def record_hour(filename, ext, stream_url):
         '-crf',
         '25',
         '-t',
-        '01:00:00',
+        record_duration,
         filename + '.' + ext
     ]
 
@@ -32,7 +32,7 @@ def record_hour(filename, ext, stream_url):
         '-c:a',
         'copy',
         '-t',
-        '01:00:00',
+        record_duration,
         filename + '.' + ext
     ]
 
@@ -62,6 +62,7 @@ if __name__ == '__main__':
             checker = "%S"
             wait_time = 2
             period = timedelta(minutes=1)
+            record_duration = '00:01:00'
 
             dt_start_string = current_dt.strftime("%Y%m%d%H%M00")
             dt_next = current_dt + period
@@ -70,6 +71,7 @@ if __name__ == '__main__':
             checker = "%M"
             wait_time = 120
             period = timedelta(hours=1)
+            record_duration = '01:00:00'
 
             dt_start_string = current_dt.strftime("%Y%m%d%H0000")
             dt_next = current_dt + period
@@ -83,7 +85,7 @@ if __name__ == '__main__':
             filename = 'DET024SkySportNews_gcp_la4_' + dt_start_string + '_' + dt_next_string
 
             try:
-                x = threading.Thread(target=record_hour, args=(filename, 'mp4', stream_url))
+                x = threading.Thread(target=record_hour, args=(filename, 'mp4', stream_url, record_duration))
                 x.start()
             except Exception as e:
                 print(e)
